d0447e5861f7b6fda637052f968d339899d4b0b9
swanf <swanf@outlook.com>
2019-11-24 20:39:26 +0100

    shopcart list added

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,62 +1,67 @@
 <template>
-  <div class="shopcart">
-    <div class="content">
-      <div class="content-left" @click="toggleList">
-        <div class="logo-wrapper">
-          <div class="logo" :class="{'highlight': totalCount>0}">
-            <i class="icon-shopping_cart" :class="{'highlight': totalCount>0}"></i>
+  <div>
+    <div class="shopcart">
+      <div class="content" @click="toggleList">
+        <div class="content-left">
+          <div class="logo-wrapper">
+            <div class="logo" :class="{'highlight': totalCount>0}">
+              <i class="icon-shopping_cart" :class="{'highlight': totalCount>0}"></i>
+            </div>
+            <div class="num" v-show="totalCount>0">{{totalCount}}</div>
           </div>
-          <div class="num" v-show="totalCount>0">{{totalCount}}</div>
+          <div class="price" :class="{'highlight': totalCount>0}">{{totalPrice}}€ </div>
+          <div class="desc">Delivery Fee {{deliveryPrice}}€ </div>
         </div>
-        <div class="price" :class="{'highlight': totalCount>0}">{{totalPrice}}€ </div>
-        <div class="desc">Delivery Fee {{deliveryPrice}}€ </div>
+        <div class="content-right">
+          <!-- :class="totalPrice>=minPrice?'enough':'not-enough'" , 换种表达-->
+          <div class="pay" :class="{'enough':totalPrice>=minPrice,'not-enough':totalPrice<minPrice}" @click="pay">{{payText}}</div>
+        </div>
       </div>
-      <div class="content-right">
-        <!-- :class="totalPrice>=minPrice?'enough':'not-enough'" , 换种表达-->
-        <div class="pay" :class="{'enough':totalPrice>=minPrice,'not-enough':totalPrice<minPrice}" @click="pay">{{payText}}</div>
+
+      <div class="ball-container">
+        <!-- <div class="ball" v-for="ball in balls" :key="ball.index" v-show="ball.show" transition="drop"> -->
+          <transition name="drop"
+            @before-enter="beforeDrop"
+            @enter="dropping"
+            @after-enter="afterDrop"
+            v-for="(ball, index) in balls" :key="index"
+            >
+            <div class="ball" v-show="ball.show">
+              <div class="inner inner-hook"></div>
+            </div>
+          </transition>
       </div>
+
+      <transition name="fold">
+        <div class="shopcart-list" v-show="listShow">
+          <div class="list-header">
+            <h1 class="title">Cart</h1>
+            <span class="empty" @click="clearCart">Clear</span>
+          </div>
+          <div class="list-content" ref="listContent">
+            <ul>
+              <li class="food" v-for="food in foodList" :key="food.name">
+                <span class="name">{{food.name}} * {{food.count}}</span>
+                <div class="price"><span>{{food.price}}€ </span></div>
+                <div class="cartcontrol-wrapper">
+                  <cartcontrol :food="food"></cartcontrol>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </transition>
     </div>
 
-    <div class="ball-container">
-      <!-- <div class="ball" v-for="ball in balls" :key="ball.index" v-show="ball.show" transition="drop"> -->
-        <transition name="drop"
-          @before-enter="beforeDrop"
-          @enter="dropping"
-          @after-enter="afterDrop"
-          v-for="(ball, index) in balls" :key="index"
-          >
-          <div class="ball" v-show="ball.show">
-            <div class="inner inner-hook"></div>
-          </div>
-        </transition>
-    </div>
-
-    <transition name="fold">
-      <div class="shopcart-list">
-        <div class="list-header">
-          <h1 class="title">Cart</h1>
-          <span class="empty" @click="clearCart">Clear</span>
-        </div>
-        <div class="list-content" ref="listContent">
-          <ul>
-            <li class="food" v-for="food in foodList" :key="food.name">
-              <span class="name">{{food.name}} * {{food.count}}</span>
-              <div class="price"><span>{{food.price}}€ </span></div>
-              <div class="cartcontrol-wrapper">
-                <cartcontrol :food="food"></cartcontrol>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
+    <transition name="fade">
+      <div class="list-mask" v-show="listShow" @click="toggleList"></div>
     </transition>
   </div>
-
 </template>
 
 <script>
-// import Vue from 'vue'
-// import BScroll from 'better-scroll'
+import Vue from 'vue'
+import BScroll from 'better-scroll'
 import cartcontrol from '../cartcontrol/cartcontrol'
 
 export default{
@@ -112,8 +117,7 @@ export default{
       } else {
         return 'Check out'
       }
-    }
-    /* 'ele' not working
+    },
     listShow () {
       if (this.totalCount === 0) { // decrease to zero, no balls
         // eslint-disable-next-line
@@ -136,7 +140,6 @@ export default{
       }
       return this.isShow
     }
-    */
   },
   methods: {
     toggleList () {
@@ -298,6 +301,7 @@ export default{
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+@import "../../common/stylus/index.styl";
 .shopcart
   position fixed
   left 0
@@ -400,4 +404,71 @@ export default{
           border-radius 50% // round ball
           background rgb(0, 160, 220)
           transition all 0.4s linear // 最后花了三个小时找的bug，竟然是因为忘记在transition的时间后面加上秒（0.4 -> 0.4s）
+  .shopcart-list
+    position absolute
+    left 0
+    top 0
+    z-index -1
+    width 100%
+    transform translate3d(0, -100%, 0) // the status of display
+    &.fold-enter-active, &.fold-leave-active
+      transition all 0.5s
+    &.fold-enter, &.fold-leave-active
+      transform translate3d(0, 0, 0)
+    .list-header
+      height 40px
+      line-height 40px
+      padding 0 18px
+      background #f3f5f7
+      border-bottom 1px solid rgba(7, 17, 27, 0.1)
+      .title
+        float left
+        font-size 14px
+        color rgb(7, 17, 27)
+      .empty
+        float right
+        font-size 12px
+        color rgb(0, 160, 220)
+    .list-content
+      padding 0 18px
+      max-height 217px
+      overflow hidden
+      background #fff
+      .food
+        position relative
+        padding 12px 0
+        box-sizing border-box
+        border-1px(rgba(7, 17, 27, 0.1))
+        .name
+          line-height 24px
+          font-size 14px
+          color rgb(7, 17, 27)
+        .price
+          position absolute
+          right 90px
+          bottom 12px
+          line-height 24px
+          font-size 14px
+          font-weight 700
+          color rgb(240, 20, 20)
+        .cartcontrol-wrapper
+          position absolute
+          right 0
+          bottom 6px
+.list-mask
+  position fixed
+  top 0
+  left 0
+  width 100%
+  height 100%
+  z-index 40
+  backdrop-filter blur(10px)
+  opacity 1
+  background rgba(7, 17, 27, 0.6)
+  &.fade-enter-active, &.fade-leave-active
+    transition all 0.5s
+  &.fade-enter, &.fade-leave-active
+    opacity 0
+    background rgba(7, 17, 27, 0)
+
 </style>
